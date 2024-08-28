d49115cdb0ec89641d5752d690333ced9aa8bdba
RedHood02 <cristian_20037@hotmail.com>
2024-08-28 18:34:07 -0400

    Added Voronoi Shadergraphs

---
+++ Assets/_Dev/_Cristian/Scripts/UI/UIDirectionController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIDirectionController : MonoBehaviour
+{
+    [SerializeField] Transform playerTransformRef;
+    [SerializeField] Transform exitTransformRef;
+    [SerializeField] Transform sphereTransformRef;
+
+
+    private void Update()
+    {
+        GetDirection();
+    }
+
+    void GetDirection()
+    {
+        if (playerTransformRef == null) return;
+
+        Vector3 direction = Vector3.Normalize(exitTransformRef.position - playerTransformRef.position);
+
+        Vector3 offset = playerTransformRef.position + 4f * direction.x * direction;
+
+        Vector3 newPos = new(offset.x, 20, offset.z);
+
+        sphereTransformRef.position = newPos;
+    }
+}
