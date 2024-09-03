e7e6a9f79a2931189d99fd7756d916cb2a2a0125
RedHood02 <cristian_20037@hotmail.com>
2024-09-03 13:05:39 -0400

    Introduction Player Radius Shadergraph

---
+++ Assets/_Dev/_Cristian/Scripts/Map/MapPlayerPosManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPlayerPosManager : MonoBehaviour
+{
+    [SerializeField] Transform playerTransformRef;
+    [SerializeField] MeshRenderer mapMeshRef;
+    [SerializeField] Material mapMaterial;
+
+    private void Awake()
+    {
+        mapMaterial = mapMeshRef.material;
+    }
+
+    private void Update()
+    {
+        mapMaterial.SetVector("_Player_Position", playerTransformRef.position);
+        
+
+    }
+}
