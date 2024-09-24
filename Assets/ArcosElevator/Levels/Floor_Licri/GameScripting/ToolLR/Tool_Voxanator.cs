6ab00ce32a25cfa22937829f509d967a9ec30486
Lightcred <lukibensa@gmail.com>
2024-09-24 19:17:38 -0300

    Tool update

--- Assets/ArcosElevator/Levels/Floor_Licri/GameScripting/ToolLR/Tool_Voxanator.cs
+++ Assets/ArcosElevator/Levels/Floor_Licri/GameScripting/ToolLR/Tool_Voxanator.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tool_Voxelator : Tool
+public class Tool_Voxanator : Tool
 {
+    public float rayLength = 10;
     [SerializeField] private LayerMask collisionLayers = 1;
     [SerializeField] private AudioClip shootSound;
     void Start()
@@ -11,20 +12,31 @@ public class Tool_Voxelator : Tool
         AddToolFunction(KeyCode.Mouse0, NormalShoot);
         AddToolFunction(KeyCode.Mouse1, Voxanada);
     }
+    private void Update()
+    {
+       // DrawRay();
+    }
 
     void NormalShoot()
     {
+        PlaySound(shootSound);
         Ray ray = new Ray(transform.position, transform.forward);
+
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * 10, Color.red, 10f);
 
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 10, collisionLayers))
         {
             Debug.Log("Le he dado a: " + hitInfo.collider.gameObject.name);
-            PlaySound(shootSound);
         }
     }
 
     void Voxanada()
     {
 
+    }
+    void DrawRay()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * rayLength, Color.green);
     }
 }
