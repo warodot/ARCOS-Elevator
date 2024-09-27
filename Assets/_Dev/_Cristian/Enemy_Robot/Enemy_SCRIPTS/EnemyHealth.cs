64f01c2344e4cecf2b82d5d8d15a37eae4790979
RedHood02 <cristian_20037@hotmail.com>
2024-09-27 13:07:48 -0300

    Added new animations

---
+++ Assets/_Dev/_Cristian/Enemy_Robot/Enemy_SCRIPTS/EnemyHealth.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : Health
+{
+    public int maxHealth = 50;
+    private int currentHealth;
+
+    [Serializable] Animator anima;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    /// <summary>
+    /// Deals damage to the object and calls onHealthChanged event
+    /// </summary>
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+
+        onHealthChanged.Invoke(currentHealth);
+    }
+
+    /// <summary>
+    /// Kills the object and calls onDeath event
+    /// </summary>
+    public void Die()
+    {
+        
+    }
+
+    /// <summary>
+    /// Sets health directly and calls onHealthChanged event
+    /// </summary>
+    public void SetHealth(int health)
+    {
+        currentHealth = health;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+}
