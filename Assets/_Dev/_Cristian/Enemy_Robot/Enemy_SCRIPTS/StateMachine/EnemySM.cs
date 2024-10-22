e0492d9546d453d93fd0d11a7422074892a3a11f
RedHood02 <cristian_20037@hotmail.com>
2024-10-22 16:13:29 -0300

    de

--- Assets/_Dev/_Cristian/Enemy_Robot/Enemy_SCRIPTS/StateMachine/EnemySM.cs
+++ Assets/_Dev/_Cristian/Enemy_Robot/Enemy_SCRIPTS/StateMachine/EnemySM.cs
@@ -260,15 +260,4 @@ public class EnemySM : StateMachine
             maxDegreesDelta: Time.deltaTime * turnRate
         );
     }
-    public void DrawDebugCircle(Vector3 center, float radius, Color color)
-    {
-        Vector3 prevPos = center + new Vector3(radius, 0, 0);
-        for (int i = 0; i < 30; i++)
-        {
-            float angle = (float)(i + 1) / 30.0f * Mathf.PI * 2.0f;
-            Vector3 newPos = center + new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
-            Debug.DrawLine(prevPos, newPos, color);
-            prevPos = newPos;
-        }
-    }
 }
